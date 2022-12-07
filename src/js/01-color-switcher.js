6df0c0ae80be05b6a2af0b056a46cd7ef55229fd
RomanPrus <skimunion@gmail.com>
2022-12-07 15:40:07 +0200

    hm9 task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let timer = null;
+
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  timer = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+});
